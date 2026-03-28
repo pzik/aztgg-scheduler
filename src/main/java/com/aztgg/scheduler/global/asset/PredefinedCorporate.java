b56054111f5f058c4edaa973ab7e4e434513b1a0
MinWoo Kim <kbsserver@naver.com>
2026-03-28 15:47:21 +0900

    feat: 크림 크롤러

--- src/main/java/com/aztgg/scheduler/global/asset/PredefinedCorporate.java
+++ src/main/java/com/aztgg/scheduler/global/asset/PredefinedCorporate.java
@@ -116,9 +116,9 @@ public enum PredefinedCorporate {
     // 센드버드
     SENDBIRD("SENDBIRD", PredefinedCompany.SENDBIRD, "센드버드"),
 
-    // 크림
-    KREAM("KREAM", PredefinedCompany.KREAM, "크림"),
-    KREAM_PAY("KREAM Pay", PredefinedCompany.KREAM, "크림페이"),
+    // 크림 (네이버 소속)
+    KREAM("KREAM", PredefinedCompany.NAVER, "크림"),
+    KREAM_PAY("KREAM Pay", PredefinedCompany.NAVER, "크림페이"),
     ;
 
     private final String id; // 각 스크랩 시 공고의 대상 법인을 식별하는 값
